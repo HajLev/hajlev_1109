b473e42f6059b3640f1ba485eab0690342408582
Levente Hajdu <hajdu.levente@verebelyszki.hu>
2020-11-09 10:25:14 +0100

    Belekezdtünk a feladatba

--- hajlev_1109/Program.cs
+++ hajlev_1109/Program.cs
@@ -7,12 +7,14 @@ using System.Threading.Tasks;
 namespace hajlev_1109
 {
 
+    //érték típus
     struct EmberS
     {
         public string nev;
         public int eletkor;
     }
 
+    //referencia típus
     class EmberC
     {
         public string nev;
@@ -24,9 +26,20 @@ namespace hajlev_1109
     {
         static void Main(string[] args)
         {
+            var s1 = new EmberS() {nev = "Zoli", eletkor = 30, };
+            var c1 = new EmberC() {nev = "Zoli", eletkor = 30, };
+
+            var s2 = s1;
+            var c2 = c1;
+
+            s2.nev = "Jolánka";
+            c2.nev = "Jolánka";
+
+            Console.WriteLine($"Nev: {s1.nev}\nKor: {s1.eletkor}");
+            Console.WriteLine($"Nev: {c1.nev}\nKor: {c1.eletkor}");
 
 
-
+            Console.ReadKey();
         }
     }
 }
